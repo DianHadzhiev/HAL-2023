12ce93769c1ec201dfc43ece267c83f71ced6084
Pieter <p.vanegmond@student.tudelft.nl>
2024-05-23 13:47:28 +0200

    Bit of javadoc

--- src/main/java/org/chat/hal2023/User.java
+++ src/main/java/org/chat/hal2023/User.java
@@ -2,8 +2,20 @@ package org.chat.hal2023;
 
 public class User {
 
+    /**
+     * String that holds the username of the user.
+     */
     private String username = "username";
+
+    /**
+     * String that holds the password of the user.
+     */
     private String password = "password";
+
+    /**
+     * String that holds the email of the user.
+     * Must be in a proper email format.
+     */
     private String email = "email@domain.net";
 
     public User() {
@@ -29,11 +41,18 @@ public class User {
         return email;
     }
 
+    /**
+     * Method to put a new email in the email field of the user.
+     * This new email must follow a proper email format.
+     * @param email the new email
+     * @return  true if the new email is put into the email field.
+     *          false if the new email isn't put into the email field.
+     */
     public boolean setEmail(String email) {
-        if(email == null) {
+        if (email == null) {
             return false;
         }
-        if(email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+        if (email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
             this.email = email;
             return true;
         }
