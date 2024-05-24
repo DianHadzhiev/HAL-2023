e510a184b86bd75db740cb9ba30dc4e88149c8a8
Pieter <p.vanegmond@student.tudelft.nl>
2024-05-24 09:52:27 +0200

    Made hardcoded login and switching scenes work

--- src/main/java/org/chat/hal2023/ChatBotApp.java
+++ src/main/java/org/chat/hal2023/ChatBotApp.java
@@ -8,40 +8,59 @@ import javafx.stage.Stage;
 
 public class ChatBotApp extends Application {
     //variables
-    private Stage mainStage;
+    //private Stage mainStage;
 
-    @Override
+    /*@Override
     public void start(Stage chatStage) throws Exception {
         this.mainStage = chatStage;
-        showChatScene();
+        showLoginScene();
+    }*/
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("LoginScherm.fxml"));
+
+        Parent loginRoot = loginLoader.load();
+        Parent chatRoot = FXMLLoader.load(getClass().getResource("ChatBot.fxml"));
+
+        Scene loginScene = new Scene(loginRoot, 800, 600);
+        Scene chatScene = new Scene(chatRoot, 800, 600);
+
+        LoginController loginController = loginLoader.getController();
+        loginController.setMainApp(this);
+        loginController.setChatScene(chatScene);
+        loginController.setPrimaryStage(primaryStage);
+
+        primaryStage.setTitle("HAL-2023");
+        primaryStage.setScene(loginScene);
+        primaryStage.show();
     }
 
 
     //hieronder staat de scene van de chatsysteem
-    public void showChatScene() throws Exception {
+/*    public void showChatScene() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ChatBot.fxml"));
         mainStage.setTitle("HAL-2023");
         mainStage.setScene(new Scene(root, 800, 600));
         mainStage.setResizable(false);
         mainStage.show();
-    }
+    }*/
 
 
     //hieronder staat de scene van de login functie
-    private void showLoginScene() throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+/*    private void showLoginScene() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginScherm.fxml"));
         Parent root = loader.load();
         LoginController loginController = loader.getController();
         loginController.setMainApp(this);
         mainStage.setTitle("Login");
-        mainStage.setScene(new Scene(root, 300, 200));
+        mainStage.setScene(new Scene(root, 600, 400));
         mainStage.show();
-    }
+    }*/
 
 
     public static void main(String[] args) {
         launch(args);
     }
-
 
 }
--- src/main/java/org/chat/hal2023/LoginController.java
+++ src/main/java/org/chat/hal2023/LoginController.java
@@ -1,12 +1,42 @@
 package org.chat.hal2023;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class LoginController {
     // functie voor inloggen die erna chatScene toont op te mainStage
 
     private ChatBotApp app;
 
+    @FXML
+    private TextField usernameInput;
+
+    @FXML
+    private TextField passwordInput;
+
+    private Stage primaryStage;
+
+    private Scene chatScene;
 
     public void setMainApp(ChatBotApp app) {
         this.app = app;
+    }
+
+    @FXML
+    private void logIn() {
+        User user = new User();
+        if (user.getUsername().equals(this.usernameInput.getText()) && user.getPassword().equals(this.passwordInput.getText())) {
+            this.primaryStage.setScene(chatScene);
+        }
+    }
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
+
+    public void setChatScene(Scene scene) {
+        this.chatScene = scene;
     }
 }
