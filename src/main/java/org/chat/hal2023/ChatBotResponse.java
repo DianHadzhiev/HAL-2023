e77ac8aa33ddec659b6a391eb867290fb72c0266
Pieter <p.vanegmond@student.tudelft.nl>
2024-05-27 12:31:49 +0200

    Removed random empty lines and a bit of checkstyle

--- src/main/java/org/chat/hal2023/ChatBotResponse.java
+++ src/main/java/org/chat/hal2023/ChatBotResponse.java
@@ -1,16 +1,17 @@
 package org.chat.hal2023;
 
-import com.google.gson.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
 public class ChatBotResponse {
+
     private JsonArray documentation;
-
-
 
     public ChatBotResponse() {
         try {
@@ -18,7 +19,6 @@ public class ChatBotResponse {
             JsonElement jsonElement = JsonParser.parseReader(new InputStreamReader(inputStream));
 
             documentation = jsonElement.getAsJsonArray();
-
 
             inputStream.close();
         } catch (IOException e) {
